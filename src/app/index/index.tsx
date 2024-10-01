c9c8a01776a3479c0974dca7cb02189ec3f158c8
carlalopesj <carlaljovito@gmail.com>
2024-10-01 19:34:37 -0300

    change: new link excel

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -21,7 +21,7 @@ export default function Index() {
 
   //const linkExt = useCallback(() => { Linking.openURL(url) },[]) /
   const linkExt = useCallback(() => {
-    Linking.openURL("https://1drv.ms/x/c/48b64a126a64263a/EWkzPGxg4olCsy9ZlcAq5xABb_KK_yp0kJsobCJ7AO-_VA?e=fHMTOp"); //Link para a planilha Excel
+    Linking.openURL("https://1drv.ms/x/c/48b64a126a64263a/EdMgvuAxqD9MqkI9j-vRSaMBK5VUBFH8MAN2gW0_Y60WEg?e=zjhVGs"); //Link para a planilha Excel
   }, []);
   const pesquisa=useCallback(()=>{
     Linking.openURL("https://docs.google.com/forms/d/e/1FAIpQLSckPn6DoZ-c5gjOIUo64suleflwuR7AA80eS-DzVLMLTAzHNw/viewform?usp=sf_link");//Link da Pesquisa de aprovação
